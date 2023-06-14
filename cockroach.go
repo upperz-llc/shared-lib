6bec44a58d771f84401bcdd89c015770ef63ab2e
Derek Duncan <dduncan@atlassian.com>
2023-06-14 14:21:53 -0500

    Add SQLDB

---
+++ cockroach.go
@@ -0,0 +1,66 @@
+package sharedlib
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type CockroachDB struct {
+	pool *pgxpool.Pool
+}
+
+func (cdb *CockroachDB) CreateDeviceTelemetry(ctx context.Context, did string, data DeviceTelemetry) error {
+	query := `INSERT INTO defaultdb.public.temperature (device_id, temperature, timestamp) VALUES (@device_id, @temperature, @timestamp)`
+	args := pgx.NamedArgs{
+		"device_id":   did,
+		"temperature": math.Round(data.Temperature*100) / 100,
+		"timestamp":   time.Unix(data.Timestamp, 0),
+	}
+
+	_, err := cdb.pool.Exec(ctx, query, args)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return err
+}
+
+func (cbd *CockroachDB) CreateUser(ctx context.Context, user User) error {
+	query := `INSERT INTO defaultdb.public.user (uid, created_at) VALUES (@uid, @notification_push, @notification_sms, @created_at)`
+	args := pgx.NamedArgs{
+		"uid":               user.UID,
+		"notification_push": user.SendPushNotification,
+		"notification_sms":  user.SendSMS,
+		"created_at":        time.Unix(time.Now().Unix(), 0),
+	}
+
+	_, err := cbd.pool.Exec(ctx, query, args)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return err
+}
+
+func NewCockroachDB(ctx context.Context) (*CockroachDB, error) {
+	dbp := os.Getenv("DB_PASS")
+	dsn := "postgresql://temporary:%s@hefty-tiger-10243.5xj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(dsn, dbp))
+	if err != nil {
+		log.Fatal("failed to create connection pool", err)
+		return nil, err
+	}
+
+	return &CockroachDB{
+		pool: pool,
+	}, nil
+
+}
--- db.go
+++ db.go
@@ -33,3 +33,38 @@ type DB interface {
 	GetUser(ctx context.Context, uid string) (*User, error)
 	GetUserNotificationSettings(ctx context.Context, uid string) (*User, error)
 }
+
+type SQLDB interface {
+	// Alarm interface
+	// AddNewAlarmToAlarmTimeline(ctx context.Context, alarm Alarm) error
+	// CreateAlarmConnection(ctx context.Context, clientID string) (*Alarm, error) // TODO : Too high level
+	// IncrementAlarmAckCheckCount(ctx context.Context, alarmID string) error
+	// UpdateAlarmAck(ctx context.Context, alarmID, userUID string) error // TODO : Too high level
+	// UpdateAlarmTimelineWithClosedAt(ctx context.Context, alarm Alarm) error
+	// CloseAlarm(ctx context.Context, alarm *Alarm) error // TODO : Too high level
+	// GetDeviceOwner(ctx context.Context, deviceID string) (string, error)
+
+	// // Alarm interface
+	// CreateAlarm(ctx context.Context, alarm *Alarm) error
+	// DeleteAlarm(ctx context.Context, alarmID string) error
+	// GetAlarm(ctx context.Context, alarmID string) (*Alarm, error)
+	// QueryAlarm(ctx context.Context, clientID string, alarmType AlarmType) (*Alarm, error)
+
+	// // Device interface
+	// AddDeviceTelemetry(ctx context.Context, deviceID string, data *DeviceTelemetry) error
+	// CreateDevice(ctx context.Context, device *Device) error
+	// // DeleteDevice(ctx context.Context, deviceID string) error
+	// GetDevice(ctx context.Context, deviceID string) (*Device, error)
+	// UpdateDeviceConnectionStatus(ctx context.Context, deviceID string, connectionStatus DeviceConnectionStatus) error
+	// UpdateDeviceFirmwareVersion(ctx context.Context, deviceID, firmwareVersion string) error
+	// UpdateDeviceOTAStatus(ctx context.Context, deviceID string, status OTAStatus, timestamp int64) error
+
+	// User interface
+	CreateUser(ctx context.Context, user User) error
+	// DeleteUser(ctx context.Context, uid string) error
+	// GetUser(ctx context.Context, uid string) (*User, error)
+	// GetUserNotificationSettings(ctx context.Context, uid string) (*User, error)
+
+	// Device Telemetry
+	CreateDeviceTelemetry(ctx context.Context, did string, data DeviceTelemetry) error
+}
