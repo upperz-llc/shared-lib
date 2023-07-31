edd056ebc994c5f8852eb677aa3624d832908f49
Derek Duncan <derekduncan@gmail.com>
2023-07-31 17:32:24 -0500

    Update

--- auth.go
+++ auth.go
@@ -1,6 +1,11 @@
 package sharedlib
 
-// type NewAuthRequest struct {
-// 	Username string `json:"username"`
-// 	Password string `json:"password"`
-// }
+// Auth placeholder
+type Auth struct {
+	ID        string `json:"id"`
+	DeviceID  string `json:"device_id,omitempty"`
+	Enabled   bool   `json:"enabled,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Password  string `json:"password,omitempty"`
+	CreatedAt string `json:"created_at,omitempty"`
+}
