b5a6a171e477777faa836011b7fe782595e6c1d6
dgduncan <derekgduncan@gmail.com>
2024-02-12 22:08:11 -0600

    update

--- sql/create_device_table.sql
+++ sql/create_device_table.sql
@@ -1,14 +1,11 @@
 CREATE TABLE public.device (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  device_id STRING(20) NOT NULL,
-  connection_status STRING(20) NULL,
+  connection_status INT NULL,
   device_type INT NOT_NULL,
-  firmware_version STRING(20) NULL,
-  monitoring_status STRING(20) NULL,
+  firmware_version STRING(30) NULL,
+  monitoring_status INT NULL,
   nickname STRING(40) NULL,
   temperature INT NULL,
   "owner" UUID NULL,
-  config UUID NULL,
-  temperature_timeline UUID NULL,
   last_seen TIMESTAMPTZ NULL,
 )
