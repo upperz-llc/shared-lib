38e3974290e0c5412c7502d0a67211953754d19c
Derek Duncan <dduncan@atlassian.com>
2024-02-14 14:21:03 -0600

    update

--- sql/create_device_table.sql
+++ sql/create_device_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE public.device (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  connection_status INT NOT_NULL,
-  device_type INT NOT_NULL,
+  connection_status INT NOT NULL,
+  device_type INT NOT NULL,
   monitoring_status INT NULL,
   firmware_version STRING(30) NULL,
-  "owner" UUID NULL,
-  created_at TIMESTAMPTZ DEFAULT now() NOT_NULL,
+  "owner" UUID NULL REFERENCES public.user(id),
+  created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
   updated_at TIMESTAMPTZ NULL
 )
