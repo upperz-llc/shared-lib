6e2ef848b460035e6898778d7056ec7682049543
Derek Duncan <dduncan@atlassian.com>
2024-03-04 14:14:39 -0600

    update device_config table

--- sql/create_device_config_table.sql
+++ sql/create_device_config_table.sql
@@ -1,7 +1,11 @@
 CREATE TABLE public.device_config (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  alert_temperature INT NULL,
-  warning_temperature INT NULL,
-  target_temperature INT NULL,
-  telemetry_interval int NULL,
+  device_id UUID NOT NULL REFERENCES public.device(id),
+  alert INT NULL,
+  warning INT NULL,
+  'target' INT NULL,
+  measurement_interval INT NULL,
+  'version' INT NULL,
+  created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+  updated_at TIMESTAMPTZ NULL
 )
